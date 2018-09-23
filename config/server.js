9b7e68bdab5d0e00273277ca9696c39a1cc46c25
Lucas Abbade <lucasabbade@Lucass-MacBook-Pro.local>
2018-09-23 17:36:42 -0300

    bug fixes

--- config/server.js
+++ config/server.js
@@ -7,29 +7,12 @@ let app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
-// function changeUndefined(req, res, next) {
-//     if (typeof req.session.data === "undefined") {
-//         req.session.data = {
-//             autorizado: false,
-//             _id: ""
-//         };
-//     }
-//     next();
-// }
-
 app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({extended: true}));
-//
-// app.use(expressSession({
-//     secret: 'hijiodsasdiisai2sjado',
-//     resave: false,
-//     saveUninitialized: false
-// }));
 
+consign({ cwd: process.cwd() + "/app" })
 consign()
     .include('app/routes')
-    // .then('config/dbConnection.js')
-    // .then('app/models')
     .then('app/controllers')
     .into(app);
 module.exports = app;
