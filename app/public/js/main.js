5394420f026e8369ee2b0f89ec32614e19b4eb59
Lucas Abbade <lucasabbade@Lucass-MacBook-Pro.local>
2018-09-24 17:16:42 -0300

    adding control bar

---
+++ app/public/js/main.js
@@ -0,0 +1,38 @@
+var mostRecentBtn = document.querySelector("#most-recent");
+var oldestBtn = document.querySelector("#oldest");
+var mostRecentActive;
+var numOfBlocks;
+
+init();
+
+function init() {
+    numOfBlocks = document.querySelector("#numOfBlocks").placeholder
+    mostRecentActive = mostRecentBtn.classList.contains("active");
+
+    console.log(`numOfBlocks: ${numOfBlocks}`);
+    console.log(`mostRecentActive: ${mostRecentActive}`);
+
+    mostRecentBtn.addEventListener("click", switchOrding);
+    oldestBtn.addEventListener("click", switchOrding);
+}
+
+function setBlocksNum() {
+    const blocks = document.querySelector("#numOfBlocks").value;
+    if (/\d+/.test(blocks)) {
+        numOfBlocks = blocks;
+    }
+
+    reload();
+}
+
+function switchOrding() {
+    mostRecentActive = !mostRecentActive;
+    mostRecentBtn.classList.toggle("active");
+    oldestBtn.classList.toggle("active");
+    setBlocksNum();
+}
+
+function reload() {
+    const ord = mostRecentActive ? "lastBlocks" : "blocks";
+    window.location.href = `http://localhost:5000/${ord}/${numOfBlocks}`;
+}
