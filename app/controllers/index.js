57596e47f8cc99f257e86e94ebe4b926e082904e
Lucas Abbade <lucasabbade@hotmail.com>
2018-09-24 17:16:42 -0300

    adding control bar

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,14 @@
 const request = require('request');
 const endpoint = 'http://localhost:6653';
+// const endpoint = 'http://35.237.108.201:6653'
 
-module.exports.renderIndex = function (application, req, res) {
-    request(`${endpoint}/lastBlocks`, (err, resp, body) => {
+module.exports.renderIndex = function (application, req, res, order, numBlocks) {
+    request(`${endpoint}/${order}/${numBlocks}`, (err, resp, body) => {
         body = JSON.parse(body);
         res.render("index", {
-            blocks: body['blocks']
+            blocks: body['blocks'],
+            order: order,
+            numBlocks: numBlocks
         });
     });
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,15 @@
 module.exports = function (application) {
     application.get('/', function (req, res) {
-        application.app.controllers.index.renderIndex(application, req, res);
+        application.app.controllers.index.renderIndex(application, req, res, "lastBlocks", "20");
+    });
+
+    application.get('/lastBlocks/:amount', (req, res) => {
+        const numBlocks = req.params.amount;
+        application.app.controllers.index.renderIndex(application, req, res, "lastBlocks", numBlocks);
+    });
+
+    application.get('/blocks/:amount', (req, res) => {
+        const numBlocks = req.params.amount;
+        application.app.controllers.index.renderIndex(application, req, res, "blocks", numBlocks);
     });
 };
