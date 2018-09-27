43aaa9411daf7fe0da5629fd7fc17fa8b99bf764
Felipemv <felipe.martinsvitor@gmail.com>
2018-09-27 16:10:15 -0300

    Remove unused routes.

--- app/routes/index.js
+++ app/routes/index.js
@@ -32,19 +32,7 @@ module.exports = function (application) {
 
 };
 
-
 // module.exports = function (application) {
-//     application.post('/register', async (req, res) => {
-//         try {
-//             console.log('POST user')
-//             console.log(json(req.body));
-//             const user = await UserInfo.create(req.body);
-//
-//             return res.send({user});
-//         } catch (err) {
-//             return res.status(400).send({error: 'Error creating new user'});
-//         }
-//     });
 
 // application.get('/', function (req, res) {
 //     application.app.controllers.index.renderIndex(application, req, res, "lastBlocks", "20");
