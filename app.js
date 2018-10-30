40a9e10b88f20a26d81795f650a07da86ed9d8a8
Lucas Abbade <lucasabbade@hotmail.com>
2018-10-30 02:12:31 -0300

    merging

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const app = require('./config/server');
-<<<<<<< HEAD
-=======
 const db = require('./app/database');
->>>>>>> 0050e5a15d4ff89f213330d05434f73ed4200e67
 const PORT = 5000;
 
 app.listen(PORT, () => {
